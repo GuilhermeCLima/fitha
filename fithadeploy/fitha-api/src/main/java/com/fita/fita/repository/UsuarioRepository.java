b585206694ce9d88760f3e6de2630bed1783a25b
unknown <icaroduartec@gmail.com>
2020-08-13 10:32:51 -0300

    Atualizar projeto para Docker

---
+++ fithadeploy/fitha-api/src/main/java/com/fita/fita/repository/UsuarioRepository.java
@@ -0,0 +1,23 @@
+package com.fita.fita.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fita.fita.model.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String UsuarioModel);
+
+	public Optional<UsuarioModel> findByUsuarioAndSenha(String usuario, String senha);
+
+	public Optional<UsuarioModel> findByEmail(String email);
+
+	public Optional<UsuarioModel> findByUsuario(String usuario);
+	
+	public Optional<UsuarioModel> findByCpf(String cpf);
+}
---
+++ fithadeploy/fitha-api/src/test/java/com/fita/fita/FitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fita.fita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
