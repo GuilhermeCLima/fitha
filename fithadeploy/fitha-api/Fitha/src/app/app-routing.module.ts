b7325c888796ebf7a8b648ba8bbcb607a7aee40d
evelyn.vanielly <evelyn.vanielly@gmail.com>
2020-08-31 09:34:02 -0300

    card teste, home com carroussel diferente e página de produtos

--- fithadeploy/fitha-api/Fitha/src/app/app-routing.module.ts
+++ fithadeploy/fitha-api/Fitha/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { QuemsomosComponent } from './quemsomos/quemsomos.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "quemsomos", component: QuemsomosComponent },
-  {path: "produtos", component: ProdutosComponent},
+  { path: "produtos", component: ProdutosComponent},
+  {
+path: 'produto-detalhe/:id',
+    component: ProdutoDetalheComponent,
+    data: { title: 'Detalhe do Produto' }
+  },
+  
 ];
 
 @NgModule({
--- fithadeploy/fitha-api/Fitha/src/app/app.module.ts
+++ fithadeploy/fitha-api/Fitha/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { QuemsomosComponent } from './quemsomos/quemsomos.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -17,11 +19,13 @@ import { ProdutosComponent } from './produtos/produtos.component';
     NavbarComponent,
     FooterComponent,
     QuemsomosComponent,
-    ProdutosComponent
+    ProdutosComponent,
+    ProdutoDetalheComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
