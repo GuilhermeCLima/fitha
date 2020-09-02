4adf551e053cbbed84f5f9588d0f1877e7afdd58
guilherme lima <guilehrme.cl.lima@gmail.com>
2020-09-02 18:59:50 -0300

    master3

--- fitha/src/app/model/Categoria.ts
+++
@@ -1,9 +0,0 @@
-import { ProdutoModel } from "./Produto";
-
-export class CategoriaModel{
-    public id:number;
-    public nome: string;
-    public produto: ProdutoModel[];
-
-
-}
--- fitha/src/app/model/UserLogin.ts
+++
@@ -1,6 +0,0 @@
-export class UsuarioLogin {
-    public nome: string;
-    public email: string;
-    public senha: string;
-    public token: string;
-}
