98363322ca6759de27c17ea32ba8d0245314e93a
guilherme lima <guilehrme.cl.lima@gmail.com>
2020-09-14 21:59:44 -0300

    vlibras

--- fitha/src/app/home/home.component.ts
+++ fitha/src/app/home/home.component.ts
@@ -15,30 +15,34 @@ export class HomeComponent implements OnInit {
   listProduto: ProdutoModel[]
   listCategoria: CategoriaModel[]
 
-  produtoService : ProdutoService
-
   constructor(
     private router: Router,
-    private categoriaService: CategoriaService
+    private categoriaService: CategoriaService,
+    private produtoService: ProdutoService
   ) { }
 
   ngOnInit(): void {
-    window.scroll(0,0)
+    window.scroll(0, 0)
     this.findAllCategoria()
+    this.findAllProduto()
+  }
+
+  findAllProduto() {
+    this.produtoService.getAllProduto().subscribe((resp: ProdutoModel[]) => {
+      this.listProduto = resp.slice(20, 28)
+    })
   }
 
   findCategoria(categoria: string) {
 
     this.produtoService.getByProdutoCategoria(categoria).subscribe((resp: ProdutoModel[]) => {
       this.listProduto = resp
-      
+
     })
-
   }
-
   findAllCategoria() {
     this.categoriaService.getAllCategoria().subscribe((resp: CategoriaModel[]) => {
-      this.listCategoria = resp
+      this.listCategoria = resp.slice(0, 6)
     })
   }
 
